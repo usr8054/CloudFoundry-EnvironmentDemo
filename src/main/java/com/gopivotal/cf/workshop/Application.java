0d0f87bd8d2ee34abd0bab4853edc19c42e3bdd6
usr8054 <avi.sati@gmail.com>
2015-12-18 14:53:06 -0600

    Update Application.java
    blank line

--- src/main/java/com/gopivotal/cf/workshop/Application.java
+++ src/main/java/com/gopivotal/cf/workshop/Application.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 public class Application {
 
     public static void main(String[] args) {
+        
         SpringApplication.run(Config.class, args);
     }
     
